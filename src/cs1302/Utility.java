c52d0fd8a2c93dc743c2023748f8adfe0b999286
Michael Cotterell <mepcotterell@gmail.com>
2019-02-25 19:32:25 -0500

    Update Utility.java

--- src/cs1302/Utility.java
+++ src/cs1302/Utility.java
@@ -9,8 +9,12 @@ public class Utility {
     /**
      * Throws a {@code NullPointerException} if the value of any
      * references in {@code o} is {@code null}. The method performs no 
-     * actions if none of the references are {@code null}.
+     * actions if none of the references are {@code null}. If {@code T}
+     * is explicitly parameterized at compile time, then the compiler will
+     * also enforce a compile-time check that all references in {@code o}
+     * are type {@code T}.
      *
+     * @param <T> the types of the references to check
      * @param method the name of the calling method.
      * @param o parameter containing the object to verify.
      * @throws NullPointerException if parameter {@code o} 
