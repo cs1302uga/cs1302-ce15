28489439fc395f8d95ef9e64360f30e91ac6ab90
bjb211 <bjb211@uga.edu>
2019-10-02 00:26:31 -0400

    Pulled Driver, Rectangle, and Square from exercise 14 for starting point

--- src/cs1302/ce15/Driver.java
+++ src/cs1302/ce15/Driver.java
@@ -1,13 +1,37 @@
 package cs1302.ce15;
 
+import java.util.Arrays;
 import cs1302.Utility;
 
 /**
- * A class to test the functionality of various generic methods.
+ * Driver class for {@code cs1302-ce15}.
  */
 public class Driver {
+
     public static void main(String[] args) {
-        //Your code here
+
+        // SOME ARRAYS FOR TESTING
+
+        Shape[] shapes = new Shape[] {
+            new Circle(2.0),
+            new Ellipse(3.2, 2.8),
+            new Rectangle(Math.PI, 4.0)
+        };
+
+        Circle[] circles = new Circle[] {
+            new Circle(4.5),
+            new Circle(1.0),
+            new Circle(2.3)
+        };
+
+        Rectangle[] rectangles = new Rectangle[] {
+            new Square(2.0),
+            new Rectangle(1.0, 4.0),
+            new Square(1.41421356237),
+            new Rectangle(2.0, 1.0)
+        };
+
+        // YOUR CODE DOWN HERE
 
     } // main
 
