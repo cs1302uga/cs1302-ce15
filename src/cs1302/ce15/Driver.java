635609265721b6f1234578ce81ef7cc922f5c628
bjb211 <bjb211@uga.edu>
2019-10-02 00:31:25 -0400

    Removed two arrays and added more objects to better fit exercise 15

--- src/cs1302/ce15/Driver.java
+++ src/cs1302/ce15/Driver.java
@@ -10,7 +10,7 @@ public class Driver {
 
     public static void main(String[] args) {
 
-        // SOME ARRAYS FOR TESTING
+        // SOME OBJECTS FOR TESTING
 
         Shape[] shapes = new Shape[] {
             new Circle(2.0),
@@ -18,18 +18,13 @@ public class Driver {
             new Rectangle(Math.PI, 4.0)
         };
 
-        Circle[] circles = new Circle[] {
-            new Circle(4.5),
-            new Circle(1.0),
-            new Circle(2.3)
-        };
+        Circle c1 = new Circle(4.5);
+        Circle c2 = new Circle(1.0);
+        Circle c3 = new Circle(2.3);
 
-        Rectangle[] rectangles = new Rectangle[] {
-            new Square(2.0),
-            new Rectangle(1.0, 4.0),
-            new Square(1.41421356237),
-            new Rectangle(2.0, 1.0)
-        };
+        Square s1 = new Square(2.0);
+        Rectangle r1 = new Rectangle(1.0, 4.0);
+        Rectangle r2 = new Rectangle(2.0, 1.0);
 
         // YOUR CODE DOWN HERE
 
